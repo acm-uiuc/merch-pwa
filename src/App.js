f855da512dffacff9906fa2d4faeaaf13493c79e
Marzuk Rashid <mail@marzuk.io>
2021-10-15 17:35:32 -0500

    basic api status check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {apiStatus: "MERCH API OFFLINE"};
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then(response => response.text())
+      .then(data => this.setState({apiStatus: data}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>{this.state.apiStatus}</code>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
